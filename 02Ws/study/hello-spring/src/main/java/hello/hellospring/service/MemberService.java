389437f7f713efca7acf751dd9c42e775731ad17
Binggrae <hoblah@naver.com>
2022-05-05 00:06:33 +0900

    회원 서비스 개발

---
+++ 02Ws/study/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,58 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    //private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    // 컨스트럭터 - 외부에서 넣어주도록 변경. 직접 new해서 생성하는것이 아님.
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원X
+
+        /*
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }); */
+
+        // 앞에 옵셔널을 지운후 바로 뒤에 메서드를 붙혀 사용가능.
+        validateDuplicateMember(member); // 중복 회원 검증.
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // Refactor This > Ctrl+Alt+Shift+T > 리팩토링 관련 전체 항목을 조회.
+    // Extract Method > Ctrl+Alt+M - 메소드 추출 리팩토링은 메소드를 더 짧고 읽기 쉽게 만들 수 있습니다.
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회 > 모든 전체 목록을 표시하는 메서드를 호출하여 리턴함.
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
---
+++ 02Ws/study/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,96 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+
+    // 테스트 메서드가 끝날때마다 객체를 지우게된다. 메서드 실행순서에 상관없이 오류가 나지않음.
+    // 테스틑 서로 의존관계없이 설계가 되어야한다.
+    // 그러기 위해선 하나의 테스트가 끝날때마다 저장소나 공용데이터들을 깔끔하게 지워줘야 문제가없다.
+    @AfterEach
+    public void afterEach(){
+        memoryMemberRepository.clearStore();
+    }
+    @Test
+    void 회원가입() {
+
+        //given  > 주어진 이름 데이터.
+        Member member = new Member();
+        member.setName("hello");
+
+        //when> 멤버의 join메서드 검증.
+        // 이름을 보내면 해당 연관된 아이디를 반환.
+        Long saveId = memberService.join(member);
+
+        //then
+
+        // 우리가 저장한게 리포지토리에 있는게 맞는지 확인위해
+        // 리포지토리를 꺼내야함 > saveId 넘기고, get으로 받는다.
+        // Optional<Member> findMember = memberService.findOne(saveId).get();
+        Member findMember = memberService.findOne(saveId).get();
+
+        // 이름이 저장한 이름과 저장되어있는 이름이 같은지 비교.
+        //Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+        // 스테틱 임폴트로 넘김. > import static org.assertj.core.api.Assertions.*; 코드 임포트 추가.
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    /* 회원가입시 핵심은 저장되는것도 중요하지만,
+       중복회원검증 로직을 잘태워서 예외가 터지는것도 봐야한다. */
+    // 중복 회원 예외 메서드 생성.
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        // 일부러 변수2개를 만들어 저장이름을 같도록 spring으로 설정.
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+
+        // 람다식 사용, 이 익셉션이 터져야하고 어떤 로직을 태울때 이것을 넣으면 예외가 터져야한다.
+        // IllegalStateException이 아닌 NullPointerException.class를 사용하면!! 테스트 실패함.
+        // assertThrows(NullPointerException.class, () -> memberService.join(member2));
+        // assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        // 메세지 검증 위해 IllegalStateException e = 로 받아 반환함. ctrl + shift + v 하면 자동생성.
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        // 메세지 받아서 메세지가 같은지 검증.
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+/*        try {
+            // 중복 익셉션 터지면 catch로!
+            memberService.join(member2);
+            fail(); // 실패!
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다. 123");
+        }
+*/
+
+
+        //then
+    }
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
