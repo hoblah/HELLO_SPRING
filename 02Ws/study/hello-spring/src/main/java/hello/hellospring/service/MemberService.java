453308cb045387ba3789e68dcc08dc61ff87df6a
Binggrae <hoblah@naver.com>
2022-05-06 23:35:32 +0900

    컴포넌트 스캔과 자동 의존관계 설정 - 2

--- 02Ws/study/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ 02Ws/study/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,7 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class MemberService {
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
     // 컨스트럭터 - 외부에서 넣어주도록 변경. 직접 new해서 생성하는것이 아님.
+    @Autowired // 주입.
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
