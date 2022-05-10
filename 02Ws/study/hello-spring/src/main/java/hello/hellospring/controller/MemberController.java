88c0359cf159cac03af80a1d78b60a20568861a2
Binggrae <hoblah@naver.com>
2022-05-10 20:58:17 +0900

    회원 웹 기능 - 등록

--- 02Ws/study/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ 02Ws/study/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,10 @@
 package hello.hellospring.controller;
-
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -23,4 +24,28 @@ public class MemberController {
     public void setMemberService(MemberService memberService){
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        // Member 임폴트시 hello.hellospring.domin인 우리가 만든 코드로 임포트!
+        Member member = new Member();
+        // 사용자가 입력한 값인 매개변수로 받은 form의 name을 새로운 member객체의 name에 세팅.
+        member.setName(form.getName());
+
+        // 입력된 이름 출력
+        System.out.println("member = " + member.getName());
+
+        // 이름이 저장된 member객체를 join메서드를 태워 회원등록실행.
+        memberService.join(member);
+
+        // 홈화면으로 보냄.
+        return "redirect:/";
+    }
+
+
 }
