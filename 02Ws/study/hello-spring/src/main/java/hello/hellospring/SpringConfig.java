32b1ab1c2e5376d62149c794aedf31e1c8733b28
Binggrae <hoblah@naver.com>
2022-05-19 23:58:25 +0900

    순수 JDBC - 2

--- 02Ws/study/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 02Ws/study/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,28 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    // @Autowired DataSource dataSource;
+
+    // DataSource 변수 설정.
+    private DataSource dataSource;
+
+    // DataSource 생성자 메서드로 빈 주입.
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +31,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
