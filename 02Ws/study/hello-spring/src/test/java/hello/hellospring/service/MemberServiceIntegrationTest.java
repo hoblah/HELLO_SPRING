6611b892e73b29cd191d11721dc11e10ca602d96
Binggrae <hoblah@naver.com>
2022-05-26 01:29:05 +0900

    JPA - 2

--- 02Ws/study/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ 02Ws/study/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -5,6 +5,7 @@ import hello.hellospring.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,7 +28,7 @@ class MemberServiceIntegrationTest {
     public void 회원가입() throws Exception {
         //Given
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring100");
         //When
         Long saveId = memberService.join(member);
         //Then
