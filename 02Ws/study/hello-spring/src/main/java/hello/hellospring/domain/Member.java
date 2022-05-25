af35e7e3bf0cc197dc8b574fad3f4b263d0444d7
Binggrae <hoblah@naver.com>
2022-05-25 23:55:56 +0900

    JPA - 1

--- 02Ws/study/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ 02Ws/study/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,23 @@
 package hello.hellospring.domain;
+import javax.persistence.*;
 
+@Entity
 public class Member {
 
+    /* import javax.persistence.Entity;의
+     @Entity 어노테이션을 사용한다.
+	 해당 어노테이션은 JPA가 관리하는 엔티티라고 표현한다.
+	 PK를 매핑해줘야한다.
+     pk는 등록시 디비에서 id값을 자동으로 생성해주는것을 아이덴티티라고 한다. */
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /* 디비에 있는 컬럼명인 username이랑 매핑되는 것이다.
+		 어노테이션을 사용하여 디비랑 매핑을 하는 것이다.
+		 이 정보로 쿼리문을 만들수 있는 것이다.
+		 JPA가 그렇게 동작 하는 것이다.
+		 import javax.persistence.*; 임폴트. */
+    @Column(name = "username")
     private String name;
 
     public Long getId() {
