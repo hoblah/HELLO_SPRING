b4e25aac2b366f976738b4e7f611254b33a1d937
Binggrae <hoblah@naver.com>
2022-05-27 00:15:53 +0900

    스프링 데이터 JPA

--- 02Ws/study/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 02Ws/study/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
+import javax.swing.*;
 
 @Configuration
 public class SpringConfig {
@@ -26,28 +27,47 @@ public class SpringConfig {
 
     // JPA 사용위해 EntityManager 를 받아야함.
     // @PersistenceContext
-    private EntityManager em;
+/*    private EntityManager em;
     @Autowired
     public SpringConfig(EntityManager em){
         this.em = em;
+    }*/
+
+    /* MemberRepository를 선언하고
+       생성자 주입을 받으면 된다.
+       그러면 스프링데이터JPA가 구현체를 만들어논게 된다. */
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        // return new MemberService(memberRepository());
+
+        // 스프링데이터JPA사용시 매개변수 memberRepository로 변경.
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+/*    @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
 
-        /* 스프링 빈 등록을 MemoryMemberRepository() 에서
+        *//* 스프링 빈 등록을 MemoryMemberRepository() 에서
            > JdbcMemberRepository()로 변경하고
-             dataSource는 스프링이 제공해준다. */
+             dataSource는 스프링이 제공해준다. *//*
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
 
         // JPA 사용위해 EntityManager 를 받아야함.
         return new JpaMemerRepository(em);
+
+        // 스프링 데이터 JPA 사용위해 해당 메서드 주석처리함.
     }
+
+    */
+
+
 }
